3c8a5215da2461ce484bd38c80e5674947750287
Giorgos Karatziolas <gkaratziolas@gmail.com>
2019-06-11 20:33:35 +0100

    Tidy-up usart driver

--- drivers/include/debug_usart.h
+++ drivers/include/debug_usart.h
@@ -1,6 +1,9 @@
 #ifndef DEBUG_USART_H_
 #define DEBUG_USART_H_
 
+#define DEBUG_USART_SUCCESS      0
+#define DEBUG_USART_ERR_NO_USART 1
+
 void debug_usart_bind(USART_TypeDef *USARTx);
 void debug_usart_unbind(void);
 void debug_usart_putchar(char c);
--- drivers/src/debug_usart.c
+++ drivers/src/debug_usart.c
@@ -1,43 +1,51 @@
 #include "stm32f4xx_usart.h"
 #include "debug_usart.h"
 
-static USART_TypeDef *debug_uart = 0;
+static USART_TypeDef *debug_usart = 0;
 
-void debug_usart_bind(USART_TypeDef *UARTx)
+void debug_usart_bind(USART_TypeDef *USARTx)
 {
-        debug_uart = UARTx;      
+        debug_usart = USARTx;
 }
 
 void debug_usart_unbind(void)
 {
-        debug_uart = 0;      
+        debug_usart = 0;      
 }
 
 void debug_usart_putchar(char c)
 {
+        if (debug_usart == 0) {
+                return DEBUG_USART_ERR_NO_USART;
+        }
         // Wait until transmit data register is empty
-        while (!USART_GetFlagStatus(debug_uart, USART_FLAG_TXE));
-        // Send a char using debug_uart
-        USART_SendData(debug_uart, c);
+        while (!USART_GetFlagStatus(debug_usart, USART_FLAG_TXE));
+        // Send a char using debug_usart
+        USART_SendData(debug_usart, c);
+        return DEBUG_USART_SUCCESS;
 }
 
 void debug_usart_print(char *s)
 {
+        if (debug_usart == 0) {
+                return DEBUG_USART_ERR_NO_USART;
+        }
         while (*s)
         {
                 debug_usart_putchar(*s++);
         }
         // Print carriage return
         debug_usart_putchar('\r');
+        return DEBUG_USART_SUCCESS;
 }
 
 char debug_usart_getchar()
 {
         char c;
         // Wait until data is received
-        while (!USART_GetFlagStatus(debug_uart, USART_FLAG_RXNE));
+        while (!USART_GetFlagStatus(debug_usart, USART_FLAG_RXNE));
         // Read received char
-        c = USART_ReceiveData(debug_uart);
+        c = USART_ReceiveData(debug_usart);
         debug_usart_putchar(c);
         return c;
 }
