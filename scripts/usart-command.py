b71a8eda7a8154ee55d05120b9d6f40b1b622838
Giorgos Karatziolas <gkaratziolas@gmail.com>
2020-04-26 22:58:36 +0100

    Update uart script to read gcode commands from file and send to printer

--- scripts/usart-command.py
+++ scripts/usart-command.py
@@ -3,12 +3,20 @@
 import serial
 import sys
 import time
+import math
 import random
 
 BAUD_RATE = 115200
 
-STARTA = 0x12;
-STARTB = 0x34;
+STARTA = 0x12
+STARTB = 0x34
+
+COMMAND_RESET       = 1
+COMMAND_LED         = 2
+COMMAND_MOVE        = 3
+COMMAND_GCODE       = 4
+COMMAND_GCODE_BUSY  = 5
+COMMAND_GCODE_ERR   = 6
 
 def serial_init(port):
 	try:
@@ -22,6 +30,17 @@ def serial_deinit(ser):
 	port_name = ser.name
 	ser.close()
 	print("closed connection to port {}".format(port_name))
+
+def send_gcode_string(ser, gcode_string):
+	ser.write((STARTA).to_bytes(1, "little"))
+	ser.write((STARTB).to_bytes(1, "little"))
+
+	ser.write((COMMAND_GCODE).to_bytes(1, "little"))
+	ser.write((len(gcode_string)).to_bytes(1, "little"))
+	for char in gcode_string:
+		byte = ord(char)
+		ser.write((byte).to_bytes(1, "little"))
+	ser.write((0x0f).to_bytes(1, "little"))
 
 def send_command(ser, command = 0x00):
 	ser.write((STARTA).to_bytes(1, "little"))
@@ -51,58 +70,34 @@ def leds_on(ser, led0, led1, led2, led3):
 	ser.write((0x0f).to_bytes(1, "little"))
 
 def read_serial(ser):
-	x = ser.read(1)
-	while(x != b''):
-		print(x)
-		x = ser.read(1)
-	print("")
+	bytes_available = ser.in_waiting
+	if bytes_available == 0:
+		return
+	x = ser.read(bytes_available)
+	print(x, len(x))
+	print()
 
-def main():
-	if len(sys.argv) < 2:
-		print("error: please specify serial port.")
+def transmit_gcode_file(ser, file_name):
+	with open(file_name, "r") as f:
+		while True:
+			line = f.readline()
+			if not line:
+				break
+			line = line.replace("\n", "")
+			send_gcode_string(ser, line)
+			time.sleep(0.1)
+			read_serial(ser)
+			print(line)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("error: please specify serial port and gcode file")
 		sys.exit(1)
 	ser = serial_init(sys.argv[1])
 	if ser == None:
 		print("error: can't open serial port {}".format(sys.argv[1]))
 		sys.exit(1)
-	try:
-		command = sys.argv[2]
-		try:
-			command = int(command, 16)
-
-			if command == 0x03:
-				move_motors(ser, int(sys.argv[3]), int(sys.argv[4]))
-				sys.exit()
-			send_command(ser, command)
-			sys.exit()
-		except ValueError:
-			print("error: second argument should be single hex byte. i.e. 0x13")
-			sys.exit(1)
-	except IndexError:
-		command = 0x00
-
-	while(1):
-		leds_on(ser, random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1))
-		read_serial(ser)
-		time.sleep(0.1)
+	
+	transmit_gcode_file(ser, sys.argv[2])
 	serial_deinit(ser)
-
-from Xlib import display
-
-ser = serial_init("/dev/ttyUSB0")
-x_old = 0
-y_old = 0
-while (1):
-	data = display.Display().screen().root.query_pointer()._data
-	x = data["root_x"]*100
-	y = data["root_y"]*100
-
-	if x_old != x or y_old != y:
-		move_motors(ser, x, y)
-		x_old = x
-		y_old = y
-
-serial_deinit(ser)
-
-#if __name__ == "__main__":
-#	main()
